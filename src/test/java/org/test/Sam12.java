75ea34bf54e2004410eb50e22a62aaf0c8fbab42
santhosh <santhoshu@softclouds.com>
2025-05-20 16:59:03 +0530

    initial commit

---
+++ src/test/java/org/test/Sam12.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Sam12 {
+
+    public void sampleTest11(){
+        System.out.println("sample testing 1");
+    }
+
+    public void sampleTest22(){
+        System.out.println("sample testing 22");
+    }
+}
